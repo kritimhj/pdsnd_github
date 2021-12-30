83ab5920eb9b0143cff9c44b96258455ceed09cf
kritimhj <kriti.mahajan@quantumblack.com>
2021-12-31 00:39:21 +0530

    refactor: made like 134 more pythonic

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -131,7 +131,7 @@ def main():
         user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        if restart.lower() is not 'yes':
             break
 
 
